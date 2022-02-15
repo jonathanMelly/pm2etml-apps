143bc06b83663fdcd771dd6c743ca143ee7abd2d
Jonathan Melly <jonathan.melly@gmail.com>
2022-02-15 15:25:17 +0000

    fix(script): put back wwwgroup/user to .env file

--- scripts/update-env.sh
+++ scripts/update-env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cp .env .env.$(date +%F_%Hh%mm%Ss)
 cp .env.example .env
+#TODO: use sed to reuse key...
 php artisan key:generate --ansi
+echo "WWWGROUP=$(id -g)" >> .env
+echo "WWWUSER=$(id -u)" >> .env
