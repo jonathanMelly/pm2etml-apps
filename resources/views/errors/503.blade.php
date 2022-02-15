0c8b5c5556669f5fd09d1c4ee389c4742d6a8ab8
Jonathan Melly <jonathan.melly@gmail.com>
2022-02-15 14:25:03 +0000

    feat(maintenance): custom maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,19 @@
+<style>
+    /*https://code-boxx.com/simple-css-error-messages/*/
+    /* THE BASE */
+.bar {
+  padding: 10px;
+  margin: 10px;
+  color: #333;
+  background: #fafafa;
+  border: 1px solid #ccc;
+}
+
+/* THE VARIATIONS */
+.info {
+  color: #204a8e;
+  background: #c9ddff;
+  border: 1px solid #4c699b;
+}
+</style>
+<div class="bar info">Site en maintenance, veuillez revenir dans quelques minutes.</div>
