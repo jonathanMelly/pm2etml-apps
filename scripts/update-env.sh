9361ac90aa47c0ec3ed7156bc064d5b464257817
Jonathan Melly <jonathan.melly@gmail.com>
2022-02-24 16:52:47 +0100

    feat(script): ps1 Added ps1 symlink to sh script for update-env

--- scripts/update-env.sh
+++ scripts/update-env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#WARNING: to be run from base project path: scripts/....sh or .ps1
+#PS1 is a symlink (New-Item -ItemType SymbolicLink -Path "update-env.ps1" -Target "update-env.sh")
 cp .env .env.$(date +%F_%Hh%mm%Ss)
 cp .env.example .env
 #TODO: use sed to reuse key...
