945a89d7b7655635f3ba153933da21a6f84c2d2e
Jonathan Melly <jonathan.melly@gmail.com>
2022-02-25 16:04:22 +0100

    feat(deploy): added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Should be called upon deployment (prod)
+#cd $PWD probably useless
+log=storage/logs/deploy-$(date +%F_%Hh%mm%Ss).log
+php='/opt/php81/bin/php'
+composer install --optimize-autoloader --no-dev --no-interaction 2>&1 >> $log
+#TODO Regenerate key ??
+$php artisan migrate --no-interaction --force 2>&1 >> $log
+$php artisan optimize:clear 2>&1 >> $log
+$php artisan optimize 2>&1 >> $log
+#done by optimize
+#$php artisan config:cache 2>&1 >> $log
+$php artisan event:cache 2>&1 >> $log
+$php artisan permission:cache-reset 2>&1 >> $log
+#done by optimize
+#$php artisan route:cache 2>&1 >> $log
+$php artisan view:cache 2>&1 >> $log
+
+#TODO put back site online [add put it offline with azure devops web hook]
+#curl ...
