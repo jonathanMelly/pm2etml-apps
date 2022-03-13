bce4c1c41c591c18077c2753d97e83be500b48f1
Jonathan Melly <jonathan.melly@gmail.com>
2022-03-13 07:45:16 +0100

    feat(ui): enable daisy themes todo: revamp generated pages #62

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,19 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("daisyui")
+    ],
+
+    daisyui: {
+        styled: true,
+        themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+        base: true,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: "",
+        darkTheme: "dark",
+    },
 };
