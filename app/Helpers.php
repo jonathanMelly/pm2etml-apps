a4702cd5bc4acb9be971385ecf1d14910ce79a31
Jonathan Melly <jonathan.melly@gmail.com>
2022-03-20 21:29:35 +0100

    fet(ordinal): draft of ordinal helper #62

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,3 +1,15 @@
 <?php
 
 //currently empty
+if(!function_exists('ordinal'))
+{
+    function ordinal(int $number):string
+    {
+        if($number>31)
+        {
+            return '';
+        }
+        return preg_replace('/\d+/','',date("S", mktime(0, 0, 0, 0, $number, 0)));
+    }
+}
+
---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_ordinal()
+    {
+        $this->assertEquals('st',ordinal(1));
+        $this->assertEquals('nd',ordinal(2));
+        $this->assertEquals('rd',ordinal(3));
+    }
+}
