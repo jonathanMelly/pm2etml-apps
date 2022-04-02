6f90e3060740607c099a098f165b4ff9bfa38216
Jonathan Melly <jonathan.melly@gmail.com>
2022-04-02 22:26:12 +0200

    feat(contract): FIX workers (students) can apply for jobs ;-) #62

---
+++ app/Http/Requests/StoreContractRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreContractRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('jobs-apply');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+            'client' => 'required|int',
+            'job_definition_id'=>'required|int'
+        ];
+    }
+}
