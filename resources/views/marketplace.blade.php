10d482295311b33edb16642f2d0322c2bff23445
Jonathan Melly <jonathan.melly@gmail.com>
2022-05-04 13:44:53 +0200

    fix(job): create button shown only for authorized user

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -2,9 +2,11 @@
 
     <div class="sm:mx-6 bg-base-200 rounded-box sm:p-3 p-1">
 
+        @can('jobDefinitions.create')
         <div class="pb-2">
             <a class="btn btn-sm" href="{{route('jobDefinitions.create')}}"><i class="fa-solid fa-plus fa-lg"></i></a>
         </div>
+        @endcan
 
         <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-1 sm:gap-2 md:gap-3">
             @forelse ($definitions as  $definition)
