7f5c8405745104fc53d3c90abcf424c27587e0cf
Jonathan Melly <jonathan.melly@gmail.com>
2022-06-07 10:18:07 +0200

    fix(deploy): fixed output redirection !!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,14 +17,14 @@ composer="$php /usr/lib64/plesk-9.0/composer.phar"
 
 #B)using local artisan
 #This will fail on FIRST deploy but as there was nothing before it’s not a problem
-$php artisan down 2>&1 >> $log
+$php artisan down >> $log 2>&1
 
 # 2>&1 doesn’t seem to work with composer....
-$composer install --optimize-autoloader --no-dev --no-interaction 2>>$log >>$log
+$composer install --optimize-autoloader --no-dev --no-interaction >>$log 2>&1
 #TODO Regenerate key ??
 
 #Backup DB
-$php artisan backup:run --only-db 2>&1 >> $log
+$php artisan backup:run --only-db >> $log 2>&1
 
 migrateCmd="migrate"
 #staging resets DB
@@ -32,17 +32,17 @@ if [ -n $1 ] && [ $1 = "staging" ]; then
     echo "resetting+seeding staging DB" >> $log
     migrateCmd="migrate:fresh --seed"
 fi
-$php artisan $migrateCmd --no-interaction --force 2>&1 >> $log
+$php artisan $migrateCmd --no-interaction --force >> $log 2>&1
 
-$php artisan optimize:clear 2>&1 >> $log
-$php artisan optimize 2>&1 >> $log
+$php artisan optimize:clear >> $log 2>&1
+$php artisan optimize >> $log 2>&1
 #done by optimize
 #$php artisan config:cache 2>&1 >> $log
-$php artisan event:cache 2>&1 >> $log
-$php artisan permission:cache-reset 2>&1 >> $log
+$php artisan event:cache >> $log 2>&1
+$php artisan permission:cache-reset >> $log 2>&1
 #done by optimize
 #$php artisan route:cache 2>&1 >> $log
-$php artisan view:cache 2>&1 >> $log
+$php artisan view:cache >> $log 2>&1
 
 #creates directory if needed
 dmz_dir="storage/dmz-assets"
@@ -52,4 +52,4 @@ if [ ! -d "${dmz_dir}" ]; then
 fi
 
 #Put back site online
-$php artisan up 2>&1 >> $log
+$php artisan up >> $log 2>&1
