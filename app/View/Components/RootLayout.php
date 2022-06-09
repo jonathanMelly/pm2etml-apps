4b87c572b56b6ae14ee84bcfdfd2949a1223141f
Jonathan Melly <jonathan.melly@gmail.com>
2022-06-09 21:23:51 +0200

    feat(footer): github issues, wakata badges and version

--- app/View/Components/RootLayout.php
+++ app/View/Components/RootLayout.php
@@ -2,14 +2,23 @@
 
 namespace App\View\Components;
 
+use Illuminate\Support\Facades\Cache;
+
 class RootLayout extends \Illuminate\View\Component
 {
+    const VERSION_FILENAME='version.txt';
 
     /**
      * @inheritDoc
      */
     public function render()
     {
-        return view('layouts.root');
+
+        $version = Cache::rememberForever('version', function () {
+            return file_exists(base_path(self::VERSION_FILENAME)) ?
+                trim(file_get_contents(base_path(self::VERSION_FILENAME))) : 'unknown';
+        });
+
+        return view('layouts.root')->with(compact('version'));
     }
 }
--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -1,40 +1,58 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{session('theme')??'light'}}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        @stack('custom-scripts')
-    </head>
-    <body class="font-sans antialiased">
+    @stack('custom-scripts')
+</head>
+<body class="font-sans antialiased">
 
-        <div class="min-h-screen bg-base-100 flex flex-col">
-            {{ $top??'' }}
+<div class="min-h-screen bg-base-100 flex flex-col">
+    {{ $top??'' }}
 
-            <main>
-                {{ $slot }}
-            </main>
+    <main>
+        {{ $slot }}
+    </main>
 
-            <div class="sm:mb-2 mb-1 sm:mt-3 mt-2">
-                <footer class="sm:mx-6 footer-center bg-base-300 text-base-content rounded-box text-xs">
-                    <div>
-                        <p>Copyright © {{date('Y')}} - All right reserved by PM2ETML</p>
-                    </div>
-                </footer>
+    <div class="sm:mb-2 mb-1 sm:mt-3 mt-2">
+        <footer class="sm:mx-6 bg-base-300 text-base-content rounded-box text-xs">
+            <div class="flex flex-row justify-center content-center gap-1">
+                <div>
+                    Copyright © {{date('Y')}} - All right reserved by PM2ETML -
+                </div>
+                <div>
+                    Version {{$version}}
+                </div>
+                @env('staging','production')
+                    <div>|</div>
+                    <a href="https://github.com/jonathanMelly/pm2etml-intranet/issues" target="_blank">
+                        <img class="h-4"
+                             src="https://img.shields.io/github/issues-raw/jonathanMelly/pm2etml-intranet?style=plastic?cacheSeconds=54000">{{--15minutes cache--}}
+                    </a>
+
+                    <a href="https://github.com/jonathanMelly/pm2etml-intranet#readme" target="_blank">
+                        <img class="h-4"
+                             src="https://wakatime.com/badge/user/bf7fcc14-d7d0-41c4-99cb-bbe8ecef41bf/project/4fb00346-5e05-4e6b-a906-57e91c256d09.svg"
+                             alt="wakatime">
+                    </a>
+                @endenv
             </div>
-        </div>
+        </footer>
+    </div>
+</div>
 
-    </body>
+</body>
 </html>
