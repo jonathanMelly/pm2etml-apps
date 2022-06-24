6cc37bd24e6a7ba1847ef73f8b3fefddccd4f3e3
Jonathan Melly <jonathan.melly@gmail.com>
2022-06-24 18:46:54 +0200

    chore(cicd): deploy with ssh to avoid side effects of plesk deploy and do not use prod branch as release tag is used as sha for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,44 +1,115 @@
 #!/bin/bash
-#Should be called upon deployment (prod)
+############# START CONFIG ############
+SHA=$1
+if [ -z "$SHA" ] || [ "$SHA" = "--help" ] ; then
+  echo "Usage: deploy.sh <SHA> [--no-interaction]"
+  exit 1
+fi
+NO_INTERACTION=$2
 
-log=storage/logs/deploy-$(date +%F_%Hh%MM%Ss).log
-php='/opt/php81/bin/php'
+################ END CONFIG ##########
 
-#TODO use already installed composer with php8... (kreativmedia)
-composer="$php /usr/lib64/plesk-9.0/composer.phar"
-
-#Set MAINTENANCE MODE
-#A)using artisan-remote (ideally this should be done before replacing files but currently azure devops webhook does not support custom json payload...)
-#website=$(basename $PWD)
-#api_url="https://$website/pilotage/commands/invoke"
-#token=$(grep PILOTAGE_API_KEY .env | awk -F'=' '{print $2}')
-#header="Content-type: application/json\nAuthorization: Bearer $token"
-#curl -X POST -H "$header" -d '{"name":"down"}' $api_url 2>&1 >> $log
-
-#B)using local artisan
-#This will fail on FIRST deploy but as there was nothing before it’s not a problem
+function reviewAndDelay()
 {
-    $php artisan down && \
+    SHORT_STAT=$(git diff --shortstat "$SHA")
 
-    # 2>&1 doesn’t seem to work with composer....
-    $composer install --optimize-autoloader --no-dev --no-interaction && \
-    #TODO Regenerate key ??
+    echo -n "Starting deploy [${SHORT_STAT} @ ${SHA}] in (press CTRL-C to cancel) : "
 
-    #Backup DB
-    $php artisan backup:run --only-db && \
+    #DELAY to let a last possibility to stop
+    for ((i=5;i>=1;i--));
+    do
+       echo -n "$i "
+       sleep 1
+    done
+    echo -e "\nGO GO GO"
+}
 
-    $php artisan migrate --no-interaction --force && \
+#MAIN BUSINESS
+#Only MODIFY THIS function to avoid script issues
+function deploy()
+{
+  reviewAndDelay
 
-    $php artisan optimize:clear && \
-    $php artisan optimize && \
-    #done by optimize
-    #$php artisan config:cache 2>&1 >> $log
-    $php artisan event:cache && \
-    $php artisan permission:cache-reset && \
-    #done by optimize
-    #$php artisan route:cache 2>&1 >> $log
-    $php artisan view:cache && \
+  log=storage/logs/deploy-$(date +%F_%Hh%MM%Ss).log
+  php='/opt/php81/bin/php'
 
-    #Put back site online
-    $php artisan up
-} >> "$log" 2>&1
+  composer=$(which composer)
+
+  #Set MAINTENANCE MODE
+  #A)using artisan-remote (ideally this should be done before replacing files but currently azure devops webhook does not support custom json payload...)
+  #website=$(basename $PWD)
+  #api_url="https://$website/pilotage/commands/invoke"
+  #token=$(grep PILOTAGE_API_KEY .env | awk -F'=' '{print $2}')
+  #header="Content-type: application/json\nAuthorization: Bearer $token"
+  #curl -X POST -H "$header" -d '{"name":"down"}' $api_url 2>&1 >> $log
+
+  #B)using local artisan
+  #This will fail on FIRST deploy but as there was nothing before it’s not a problem
+  {
+      $php artisan down && \
+
+      # 2>&1 doesn’t seem to work with composer....
+      $composer install --optimize-autoloader --no-dev --no-interaction && \
+      #TODO Regenerate key ??
+
+      #Backup DB
+      $php artisan backup:run --only-db && \
+
+      $php artisan migrate --no-interaction --force && \
+
+      $php artisan optimize:clear && \
+      $php artisan optimize && \
+      #done by optimize
+      #$php artisan config:cache 2>&1 >> $log
+      $php artisan event:cache && \
+      $php artisan permission:cache-reset && \
+      #done by optimize
+      #$php artisan route:cache 2>&1 >> $log
+      $php artisan view:cache && \
+
+      #Put back site online
+      $php artisan up
+  } >> "$log" 2>&1
+}
+##END MAIN BUSINESS
+
+function confirmDeploy()
+{
+  echo "######################################################################################"
+  echo "# READY TO DEPLOY following CHANGES for rev ${SHA} #"
+  echo "######################################################################################"
+  git diff "$SHA" --compact-summary
+
+  echo ""
+
+  read -r -p "Do you really want to DEPLOY this ? [y/N] " response
+  case "$response" in
+      [yY][eE][sS]|[yY])
+          deploy
+          ;;
+      *)
+          echo "Operation cancelled by user"
+          ;;
+  esac
+
+}
+
+#Check if script has been modified (checkout and start other version... which has another path so $0 won’t match a second time)
+#Remove ./ if present to match git diff and detect if script has been modified
+DEPLOY_SCRIPT_CLEAN=$(echo "$0"| sed -e s~^\./~~)
+UPDATED_REPO=".repo-$SHA"
+echo "Fetching origin" && git fetch && git diff --stat "$SHA" | grep "$DEPLOY_SCRIPT_CLEAN"
+LAST=$?
+if [ $LAST -eq 0 ]; then
+  echo "/!\DEPLOY SCRIPT UPDATE DETECTED - RUNNING UPDATED VERSION/!\ "
+  git worktree add -q "$UPDATED_REPO" "$SHA" && \
+    bash "$UPDATED_REPO/$0" "$@" && git worktree remove "$UPDATED_REPO"
+else
+  #No changes in deploy script, we can continue with that script
+  if [ "$NO_INTERACTION" = "--no-interaction" ] ; then
+    deploy
+  else
+    confirmDeploy
+  fi
+fi
+
