53b94a7658782af87b1fcaf7a8d8d874f3040129
Jonathan Melly <jonathan.melly@gmail.com>
2022-11-05 10:04:55 +0100

    chore(mariadb): update docker cmd with names and volume persistence...

--- scripts/docker-mysql.sh
+++ scripts/docker-mysql.sh
@@ -1,2 +1,2 @@
-docker run --detach --env MARIADB_ROOT_PASSWORD=123 -p3306:3306  mariadb:10.3.34
-#And then docker exec -it ee2324aff825 /bin/mysql -u root -p => create database pm2etml_intranet
+docker run --name pm2etml-mariadb --detach --env MARIADB_ROOT_PASSWORD=123 -p3306:3306 -v pm2etml-db:/var/lib/mysql  mariadb:10.3.34
+#And then docker exec -it pm2etml-mariadb /bin/mysql -u root -p => create database pm2etml_intranet
