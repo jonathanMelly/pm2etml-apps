0d8e26f60eb0f003adb2bba48fb45f069fb75b1e
Jonathan Melly <jonathan.melly@gmail.com>
2022-12-22 23:02:46 +0100

    fix(sso-bridge): avoid double json_encode of sso data
    fixes #61

--- doc/sso-bridge/callback.php
+++ doc/sso-bridge/callback.php
@@ -2,15 +2,15 @@
 require_once("sso-config.php");
 
 $ssoResult = file_get_contents(SSO_PORTAL."bridge/check?correlationId=".$_SESSION[SESSION_SSO_KEY]);
-$loginInfos=json_decode($ssoResult);
+$loginInfos=json_decode($ssoResult,true);
 
-if(!array_key_exists($loginInfos["error"]))
+if(!array_key_exists("error",$loginInfos))
 {
     //TODO LOGIN user in your app with $email (select * from users where email=$email)...
     echo "Welcome ".$loginInfos["username"].", you are authenticated (email:".$loginInfos["email"]." )";
 }
 else{
-    die($loginInfos["error"]);
+    die("Login error: ".$loginInfos["error"]);
 }
 
 
