10d6c8f0860f0d77f9a02d50d2b7de461548fa75
Jonathan Melly <jonathan.melly@gmail.com>
2022-12-22 22:02:46 +0100

    chore(test): fix DeployControllerTest.php

--- tests/Feature/Http/Controllers/DeployControllerTest.php
+++ tests/Feature/Http/Controllers/DeployControllerTest.php
@@ -4,12 +4,16 @@ beforeEach(function (){
     /* @var $this \Tests\TestCase */
     $this->beforeApplicationDestroyed(function(){
         \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+        //\Illuminate\Support\Facades\Artisan::call('up');
     });
 });
 
-test('Call optimize', function () {
+test('Call optimize with site up as admin', function () {
     /* @var $this \Tests\TestCase */
     /* @var $response \Illuminate\Testing\TestResponse */
+
+    $this->createUser(true,'root');
+
     $response = $this->get('deploy/optimize');
 
     $response->assertStatus(200);
