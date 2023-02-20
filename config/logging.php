6aa47e0e325c4b387583ee75306b885584fbc88f
Jonathan Melly <jonathan.melly@gmail.com>
2023-02-20 16:35:26 +0100

    fix(sentry): only report warnings and upper

--- config/logging.php
+++ config/logging.php
@@ -126,7 +126,7 @@ return [
             'driver' => 'sentry',
             // The minimum logging level at which this handler will be triggered
             // Available levels: debug, info, notice, warning, error, critical, alert, emergency
-            'level' => env('LOG_LEVEL', 'warning'),
+            'level' => env('LOG_LEVEL_SENTRY', 'warning'),
             'bubble' => true, // Whether the messages that are handled can bubble up the stack or not
         ],
     ],
