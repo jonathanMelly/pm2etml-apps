44436b61200de978cb8baee4b1a477e1ec6a8036
Shift <shift@laravelshift.com>
2024-06-17 10:50:48 +0000

    Use Faker methods
    Accessing Faker properties was deprecated in Faker 1.14.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,8 @@ class UserFactory extends Factory
         $fn = $this->faker->firstName();
         $ln = $this->faker->lastName();
 
-        //$email = $fn.$ln.'@'.$faker->safeEmailDomain;
-        $email = $this->faker->unique()->email; // strtolower($fn).'.'.strtolower($ln).'@eduvaud.ch';
+        //$email = $fn.$ln.'@'.$faker->safeEmailDomain();
+        $email = $this->faker->unique()->email(); // strtolower($fn).'.'.strtolower($ln).'@eduvaud.ch';
 
         return [
             'username' => 'p'.$this->faker->randomLetter().$this->faker->randomNumber(2).$this->faker->lexify('???').'@eduvaud.ch',
