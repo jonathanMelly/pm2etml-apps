542a4624470866ba426c24ad974e4d558b1a5ccf
Shift <shift@laravelshift.com>
2024-06-17 10:51:13 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Console\Commands\EvaluationReportCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// Schedule::command('inspire')->hourly();
+Schedule::command(EvaluationReportCommand::class)->dailyAt('07:45')->weekdays()
+    ->sentryMonitor('pm2etml-intranet-cron-evaluation-report');
