cbc398b5950dd24739a6c0d40f9f0cce6b41e0b0
XCarrel <Xavier.Carrel@eduvaud.ch>
2024-12-24 11:56:24 +0100

    feat(dashboard) Show unconfirmed jobs

--- resources/views/components/worker-contract-list-element.blade.php
+++ resources/views/components/worker-contract-list-element.blade.php
@@ -1,11 +1,11 @@
 @php
-    $progress = $contract->getProgress();
-    $progressPercentage = $progress['percentage'];
-    $remainingDays = $progress['remainingDays'];
+$progress = $contract->getProgress();
+$progressPercentage = $progress['percentage'];
+$remainingDays = $progress['remainingDays'];
 
-    /* @var $contract \App\Models\Contract */
-    /* @var $wc \App\Models\WorkerContract*/
-    $wc = $contract->workerContract(auth()->user()->groupMember())->firstOrFail();
+/* @var $contract \App\Models\Contract */
+/* @var $wc \App\Models\WorkerContract*/
+$wc = $contract->workerContract(auth()->user()->groupMember())->firstOrFail();
 
 @endphp
 <tr>
@@ -22,42 +22,45 @@
                     <span class="indicator-item indicator-start badge badge-primary -mt-2 text-xs">{{__('new')}}</span>
                     @endif
                     <div class="lg:font-bold lg:text-base text-xs">{{Str::words($contract->jobDefinition->title,3)}}
-                    @if($wc->name!="")
+                        @if($wc->name!="")
                         ({{$wc->name}})
-                    @endif
+                        @endif
                     </div>
                 </div>
+                @if($wc->application_status > 0)
+                <div class="indicator-item indicator-start badge badge-warning -mt-2 text-xs" title="Votre engagement n'est pas encore confirmé. Il ne s'agit pour l'instant que d'un souhait de priorité {{ $wc->application_status }}">{{__('Not confirmed')}}</div>
+                @endif
             </div>
         </a>
     </td>
     <td>
         {{collect($contract->clients)->transform(fn ($user)=>$user->getFirstnameL())->join(',')}}
         @if(!$wc->alreadyEvaluated())
-            <i onclick="switchClient{{$wc->id}}.showModal()" class="fa-solid fa-edit hover:cursor-pointer"></i>
-            <dialog id="switchClient{{$wc->id}}" class="modal">
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg">{{__('Switch client')}}</h3>
-                    <p class="py-4">{{__('Select new client')}}</p>
-                    <form method="post" action="{{route('contracts.update',[$contract])}}" id="contract-{{$contract->id}}-form"
-                          x-on:submit.prevent>
-                        @method('PATCH')
-                        @csrf
-                        <x-client-select name="clientId" :selected="$contract->clients->first()->id" :job-definition="$contract->jobDefinition" :with-stats="true" />
+        <i onclick="switchClient{{$wc->id}}.showModal()" class="fa-solid fa-edit hover:cursor-pointer"></i>
+        <dialog id="switchClient{{$wc->id}}" class="modal">
+            <div class="modal-box">
+                <h3 class="font-bold text-lg">{{__('Switch client')}}</h3>
+                <p class="py-4">{{__('Select new client')}}</p>
+                <form method="post" action="{{route('contracts.update',[$contract])}}" id="contract-{{$contract->id}}-form"
+                    x-on:submit.prevent>
+                    @method('PATCH')
+                    @csrf
+                    <x-client-select name="clientId" :selected="$contract->clients->first()->id" :job-definition="$contract->jobDefinition" :with-stats="true" />
+                </form>
+                <div class="modal-action">
+
+                    <button class="btn btn-success" onclick="spin('saveButton{{$contract->id}}');document.querySelector('#contract-{{$contract->id}}-form').submit()">
+                        <span id="saveButton{{$contract->id}}" class="hidden"></span>
+                        {{__('Save')}}
+                    </button>
+
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn btn-error">{{__('Cancel')}}</button>
                     </form>
-                    <div class="modal-action">
-
-                        <button class="btn btn-success" onclick="spin('saveButton{{$contract->id}}');document.querySelector('#contract-{{$contract->id}}-form').submit()">
-                            <span id="saveButton{{$contract->id}}" class="hidden"></span>
-                            {{__('Save')}}
-                        </button>
-
-                        <form method="dialog">
-                            <!-- if there is a button in form, it will close the modal -->
-                            <button class="btn btn-error">{{__('Cancel')}}</button>
-                        </form>
-                    </div>
                 </div>
-            </dialog>
+            </div>
+        </dialog>
         @endif
     </td>
     <td>
@@ -71,14 +74,14 @@
     </td>
     {{-- EFFORT --}}
     <td class="text-center">
-    {{$wc->getAllocationDetails()}}
+        {{$wc->getAllocationDetails()}}
     </td>
     <td class="text-center">
         @if($progressPercentage<100)
             {{$remainingDays}} {{$remainingDays>1?__('days'):__('day')}}
-        @else
+            @else
             <i class="fa-solid fa-flag-checkered"></i>
-        @endif
+            @endif
     </td>
     <x-contract-list-element-evaluation :contract="$contract" />
 </tr>
