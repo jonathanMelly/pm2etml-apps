8350eae00fe2895a521ee1a3fe9cbf1418558b89
XCarrel <Xavier.Carrel@eduvaud.ch>
2024-12-25 10:58:44 +0100

    feat(application): Provide a button to teachers who have pending applications

--- resources/views/components/nav-links.blade.php
+++ resources/views/components/nav-links.blade.php
@@ -1,18 +1,17 @@
 @props(['li'=>false])
 
 @php
-    $links = [
-        'dashboard'=> __('Dashboard'),
-        'marketplace'=> __('Market place')
-    ];
+$links = [
+'dashboard'=> __('Dashboard'),
+'marketplace'=> __('Market place')
+];
+if (auth()->user()->hasPendingContractApplications()) $links['applications'] = __('Pending applications');
 @endphp
 
 @foreach($links as $route=>$label)
-    <x-nav-link :li="$li" :href="route($route)" :active="request()->routeIs($route)"
-                class="btn btn-ghost normal-case mx-4"
-                onclick="spin('{{$route}}Button')">
-        <span class="hidden" id="{{$route}}Button"></span>{{ $label }}
-    </x-nav-link>
+<x-nav-link :li="$li" :href="route($route)" :active="request()->routeIs($route)"
+    class="btn btn-ghost normal-case mx-4"
+    onclick="spin('{{$route}}Button')">
+    <span class="hidden" id="{{$route}}Button"></span>{{ $label }}
+</x-nav-link>
 @endforeach
-
-
