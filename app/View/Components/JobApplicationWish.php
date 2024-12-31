a668186010908d22e77bf3259672e9f0b838c596
Jonathan Melly <jonathan.melly@eduvaud.ch>
2024-12-31 08:53:10 +0100

    fix(hire): fixed filename

--- app/View/Components/JobApplicationWish.php
+++ app/View/Components/JobApplicationWish.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class job-application-wish extends Component
+class JobApplicationWish extends Component
 {
     /**
      * Create a new component instance.
