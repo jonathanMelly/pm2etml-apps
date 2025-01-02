a00988db67c239f34c527f3f13977188bb054e01
Jonathan Melly <jonathan.melly@gmail.com>
2025-01-02 08:34:01 +0100

    refactor(fulleval db scripts): remove redundant infos

--- database/migrations/2024_12_12_091553_add_fullevaluation_tables.php
+++ database/migrations/2024_12_12_091553_add_fullevaluation_tables.php
@@ -6,44 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class AddEvaluationTables extends Migration
 {
-    /**
-     * Exécuter les migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
-        // Création de la permission
         \Spatie\Permission\Models\Permission::create(['name' => 'evaluation.storeEvaluation']);
 
-        // Table evaluations
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('evaluator_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('class_id')->constrained('group_names')->onDelete('restrict'); // Ajout du champ class_id
-            $table->foreignId('job_definitions_id')->constrained('job_definitions')->onDelete('cascade')->onUpdate('cascade'); // Ajout du champ job_definitions_id
+            $table->foreignId('class_id')->constrained('group_names')->onDelete('restrict');
+            $table->foreignId('job_definitions_id')->constrained('job_definitions')->onDelete('cascade')->onUpdate('cascade');
             $table->text('student_remark')->nullable();
             $table->timestamps();
 
-            // Ajout de la contrainte unique
             $table->unique(['evaluator_id', 'student_id', 'class_id', 'job_definitions_id'], 'evaluations_unique');
         });
 
-        // Table appreciations
         Schema::create('appreciations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('evaluation_id')->constrained('evaluations')->onDelete('cascade');
             $table->date('date');
-            $table->tinyInteger('level')->unsigned(); // Ajout de la colonne 'level'
+            $table->tinyInteger('level')->unsigned();
             $table->timestamps();
 
-            // Ajout des index
-            $table->index('evaluation_id', 'idx_evaluation_id');
-            $table->index('date', 'idx_date');
+            $table->index('date');
         });
 
-        // Table criterias
         Schema::create('criterias', function (Blueprint $table) {
             $table->id();
             $table->foreignId('appreciation_id')->constrained('appreciations')->onDelete('cascade');
@@ -51,12 +40,10 @@ class AddEvaluationTables extends Migration
             $table->tinyInteger('value');
             $table->boolean('checked')->default(false);
             $table->text('remark')->nullable();
-            $table->unsignedInteger('position'); // Ajout de la colonne 'position'
+            $table->unsignedInteger('position');
             $table->timestamps();
 
-            // Ajout des index
-            $table->index('appreciation_id', 'idx_appreciation_id');
-            $table->index('checked', 'idx_checked');
+            $table->index('checked');
         });
 
         Schema::create('default_criterias', function (Blueprint $table) {
@@ -65,8 +52,8 @@ class AddEvaluationTables extends Migration
             $table->string('category');
             $table->text('description');
             $table->timestamps();
-            $table->unsignedBigInteger('user_id')->nullable(); // Permet les valeurs nulles pour les critères par défaut
-            $table->integer('position'); // Ajout de la colonne position
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('position');
         });
     }
 
@@ -77,10 +64,8 @@ class AddEvaluationTables extends Migration
      */
     public function down()
     {
-        // Suppression de la permission en cas de rollback
         \Spatie\Permission\Models\Permission::where('name', 'evaluation.storeEvaluation')->delete();
 
-        // Suppression des tables
         Schema::dropIfExists('criterias');
         Schema::dropIfExists('appreciations');
         Schema::dropIfExists('evaluations');
