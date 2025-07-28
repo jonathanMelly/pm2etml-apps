1108e1d229b02dececd53b3299f83a96044b0170
Jonathan Melly <jonathan.melly@gmail.com>
2025-07-28 17:05:17 +0200

    feat(cleanup): removed apparent dead code

--- app/Models/WorkerContractAssessment.php
+++ app/Models/WorkerContractAssessment.php
@@ -6,8 +6,8 @@ use App\Constants\AssessmentState;
 use App\Services\AssessmentStateMachine;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use InvalidArgumentException;
 
 class WorkerContractAssessment extends Model
 {
@@ -25,60 +25,29 @@ class WorkerContractAssessment extends Model
         return $wc->contract->clients->first();
     }
 
-    public function student()
+    public function student(): User
     {
         /* @var $wc WorkerContract */
         $wc = $this->WorkerContract()->firstOrFail();
-        return $wc->contract->workers->first();
+        return $wc->contract->workers->first()->user;
     }
 
-    public function assessments()
+    public function assessments(): HasMany
     {
         return $this->hasMany(AssessmentCriterion::class);
     }
 
+
     /**
+     * TODO : non utilisée, laissé à titre d'exemple pour une query avec relation...
+     *
      * Retourne toutes les évaluations faites par un professeur pour un étudiant donné
      */
     public static function getAssessments($studentId)
     {
 
         return WorkerContractAssessment::query()
-            ->whereRelation('contract.groupMember.user','id','=',$studentId);
-    }
-
-    /**
-     * Crée une évaluation pour un étudiant avec un statut initial "not_evaluated"
-     */
-    public static function createAssessment($comment = null)
-    {
-        return self::create([
-            'comment' => $comment,
-            'status' => AssessmentState::NOT_EVALUATED->value // Statut initial
-        ]);
-    }
-
-    /**
-     * Récupère tous les états traversés sous forme de tableau
-     */
-    public function getStatusHistory(): array
-    {
-        return explode(',', $this->status ?? '');
-    }
-
-    /**
-     * Récupère l'état actuel sous forme d'énumération
-     */
-    public function getCurrentStatus(): AssessmentState
-    {
-        $statusHistory = $this->getStatusHistory();
-        $lastStatus = end($statusHistory);
-
-        if ($lastStatus) {
-            return AssessmentState::from($lastStatus);
-        }
-
-        throw new InvalidArgumentException('Aucun état trouvé pour cette évaluation.');
+            ->whereRelation('workerContract.contract.workers.user','id','=',$studentId);
     }
 
     /**
@@ -97,6 +66,7 @@ class WorkerContractAssessment extends Model
     }
 
     /**
+     * TODO récupérer le rôle de la session (user()->)
      * Effectue une transition vers un nouvel état
      */
     public function transition(string $role): bool
@@ -121,96 +91,7 @@ class WorkerContractAssessment extends Model
      */
     public function criteria()
     {
-        return $this->hasMany(AssessmentCriterion::class, 'appreciation_id');
-    }
-
-    /**
-     * Retourne toutes les appréciations pour une évaluation donnée
-     */
-    public static function getAppreciationsByEvaluation($evaluationId)
-    {
-        return self::where('evaluation_id', $evaluationId)->get();
-    }
-
-    /**
-     * Crée une appréciation pour une évaluation spécifique
-     */
-    public static function createAppreciation($evaluationId, $date)
-    {
-        return self::create([
-            'evaluation_id' => $evaluationId,
-            'date' => $date
-        ]);
-    }
-
-    /**
-     * Mise à jour de la date et du niveau d'une appréciation
-     */
-    public function updateAppreciation($newDate, $newLevel)
-    {
-        $this->date = $newDate;
-        $this->level = $newLevel;
-        $this->save();
-    }
-
-
-    /**
-     * Supprimer une appréciation en fonction de son ID
-     */
-    public static function deleteAppreciationById($id)
-    {
-        return self::where('id', $id)->delete();
-    }
-
-    /**
-     * Récupère une appréciation par sa date et niveau pour une évaluation spécifique
-     */
-    public static function getAppreciationByDateAndLevel($date, $level, $evaluationId)
-    {
-        return self::where('date', $date)
-            ->where('evaluation_id', $evaluationId)
-            ->where('level', $level)
-            ->first();
-    }
-
-    /**
-     * Vérifie s'il existe une appréciation pour une évaluation et une date spécifiques avec un niveau donné
-     */
-    public static function hasAppreciation($evaluationId, $date, $level)
-    {
-        return self::where('evaluation_id', $evaluationId)
-            ->where('date', $date)
-            ->where('level', $level)
-            ->exists();
-    }
-
-    public function isFullySigned(): bool
-    {
-        $signatures = $this->signatures ?? ['teacher' => false, 'student' => false];
-        return $signatures['teacher'] && $signatures['student']; // Vrai si les deux ont signé
-    }
-
-    public function addSignature(string $role): void
-    {
-        if (!in_array($role, ['teacher', 'student'])) {
-            throw new \InvalidArgumentException('Role must be either "teacher" ou "student".');
-        }
-
-        $signatures = $this->signatures ?? ['teacher' => false, 'student' => false];
-        $signatures[$role] = true; // Met le rôle à "true" pour indiquer qu'il a signé
-        $this->signatures = $signatures;
-        $this->save();
-    }
-
-
-    public function hasSigned(string $role): bool
-    {
-        if (!in_array($role, ['teacher', 'student'])) {
-            throw new \InvalidArgumentException('Role must be either "teacher" or "student".');
-        }
-
-        $signatures = $this->signatures ?? ['teacher' => false, 'student' => false];
-        return $signatures[$role]; // Retourne vrai ou faux
+        return $this->hasMany(AssessmentCriterion::class);
     }
 
 }
